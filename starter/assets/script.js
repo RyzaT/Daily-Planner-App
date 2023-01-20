2de5ea9376ec84bd92124eea6720bd0c602649b3
Ryan Taylor <ryantaylor50@hotmail.com>
2023-01-20 16:15:28 +0000

    Button to save to local storage

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -12,6 +12,30 @@ $(document).ready(function () {
         localStorage.clear();
     });
 
+    // Hour from each time slot compared to actual time
+    $(".time-div").each(function () {
+        var timeDiv = $(this).attr("id").split("-")[1];
+
+        if (currentHour == timeDiv) {
+            $(this).addclass("present");
+            $(this).children(".description").addclass("white-text");
+        }   else if (currentHour < timeDiv) {
+            $(this).removeClass("present");
+            $(this).addclass("future");
+        }   else if (currentHour > timeDiv) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+    });
+
+    // local storage when the save button is clicked
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        var value = $(this).siblings(".time-block").val();
+        var time = $(this).parent().atrr("id").split("-")[1];
+        localStorage.setItem(time, value);
+    });
+
 })
 
 
@@ -22,4 +46,3 @@ $(document).ready(function () {
 
 // enter an event when they click a timeblock.
 
-// local storage when the save button is clicked

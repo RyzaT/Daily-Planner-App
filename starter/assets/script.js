2d9ce7b60d65cb12ec1680ba84a8f01a2ea54bfe
Ryan Taylor <ryantaylor50@hotmail.com>
2023-01-20 16:51:02 +0000

    retrieve stored items from local storage attempt

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -17,11 +17,11 @@ $(document).ready(function () {
         var timeDiv = $(this).attr("id").split("-")[1];
 
         if (currentHour == timeDiv) {
-            $(this).addclass("present");
-            $(this).children(".description").addclass("white-text");
+            $(this).addClass("present");
+            $(this).children(".description").addClass("white-text");
         }   else if (currentHour < timeDiv) {
             $(this).removeClass("present");
-            $(this).addclass("future");
+            $(this).addClass("future");
         }   else if (currentHour > timeDiv) {
             $(this).removeClass("future");
             $(this).addClass("past");
@@ -32,9 +32,20 @@ $(document).ready(function () {
     $(".saveBtn").click(function (event) {
         event.preventDefault();
         var value = $(this).siblings(".time-block").val();
-        var time = $(this).parent().atrr("id").split("-")[1];
+        var time = $(this).parent().attr("id").split("-")[1];
         localStorage.setItem(time, value);
     });
+
+    // Pull items from local storage and return to correct hour.
+    $("#hour-09 .timeblock").val(localStorage.getItem("09"));
+    $("#hour-10 .timeblock").val(localStorage.getItem("10"));
+    $("#hour-11 .timeblock").val(localStorage.getItem("11"));
+    $("#hour-12 .timeblock").val(localStorage.getItem("12"));
+    $("#hour-13 .timeblock").val(localStorage.getItem("13"));
+    $("#hour-14 .timeblock").val(localStorage.getItem("14"));
+    $("#hour-15 .timeblock").val(localStorage.getItem("15"));
+    $("#hour-16 .timeblock").val(localStorage.getItem("16"));
+    $("#hour-17 .timeblock").val(localStorage.getItem("17"));
 
 })
 

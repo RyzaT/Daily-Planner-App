61b9167c1dc04faeb2444444375821be5aa47cdc
Ryan Taylor <ryantaylor50@hotmail.com>
2023-01-20 15:31:00 +0000

    Added Timeblock to html

---
+++ starter/assets/script.js
@@ -0,0 +1,9 @@
+// Display the current day at the top of the calendar
+
+// Present timeblocks for standard business hours
+
+// Color-code each timeblock based on past, present, and future
+
+// enter an event when they click a timeblock.
+
+// local storage when the save button is clicked
